41e5ef2b902b6cdb62c31915f5116743fbe6c231
Catherine Adegoke <cmadegoke@gmail.com>
2019-12-19 14:47:30 -0500

    questions added

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+      question: "Name a four letter body part which starts with H.",
+      choices: ["Eye","Knee","Feet","Nose","Head"],
+      answer: "Head"
+    },
+    {
+        question: "What country is not in the North American Continent",
+        choices: ["Canada","Cuba","Mexico","Puerto rico ","Ghana"],
+        answer: "Ghana"
+      },
+      {
+        question: "what is a bad Job for someone who is accident prone?",
+        choices: ["App developer","QA engineer","Surgeon","Ebay seller","School Teacher"],
+        answer: "Surgeon"
+      },
+      {
+        question: "what would a customer do to NOT annoy a waitress?",
+        choices: ["Snap fingers","Tip Big","Send food back","Spill drinks"],
+        answer: "Tip Big"
+      },
+      {
+        question: "Name Something that siblings do not compete over",
+        choices: ["Punishments","Grades","Parents affections","Money","Car keys"],
+        answer: "Punishments"
+      },
+  ];
