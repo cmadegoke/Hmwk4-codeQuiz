1320f7a0687d8b386ea48d9c4ea6f66dae46abc3
Catherine Adegoke <cmadegoke@gmail.com>
2019-12-20 16:03:11 -0500

    js script updated

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 // create variables to reference all DOM elements we're working with
+
   
   // time-left
+  var timeLeft = document.querySelector('#time-left');
+
+  let currentQuestion;
+  let currentQuestionIndex = 0;
+  
 
   // start game button
+  var startBtn = document.querySelector ('#startQuiz');
   // start-screen element
 
   // quiz-content
+  var questionElement = document.querySelector ('#question');
+  var choicesEl = document.querySelector ('#choices');
+  var choiceA = document.querySelector('#A');
+  var choiceB = document.querySelector('#B');
+  var choiceC= document.querySelector('#C');
+  var choiceD = document.querySelector('#D');
+  var choiceE = document.querySelector('#E');
 
   // post-game-screen
   // user-score
@@ -14,11 +28,71 @@
 
 // create variables for game logic
   // timerIntervalId
+  var timerId;
   // score
+  var score = 0;
   // secondsLeft
+  let secondsLeft;
 
 
 // create function to start game
+function startBtnHandler (){
+  console.log('start button pressed')
+  choiceA.addEventListener('click', () => clickChoice(0))
+choiceB.addEventListener('click', () => clickChoice(1))
+choiceC.addEventListener('click', () => clickChoice(2))
+choiceD.addEventListener('click', () => clickChoice(3))
+choiceE.addEventListener('click', () => clickChoice(4))
+  clearInterval (timerId);
+  secondsLeft = 300;
+  timeLeft.textContent= secondsLeft;
+
+  timerId=setInterval (function(){
+    secondsLeft --;
+    timeLeft.textContent = secondsLeft;
+
+    if (secondsLeft<= 0){
+      stopGame ();
+      
+    }
+  }, 1000);
+  displayQuestion(currentQuestionIndex)
+  // printGameScreen ();
+
+}
+function clickChoice(x) {
+  console.log(`${x} clicked`)
+  currentQuestionIndex++
+
+   if (currentQuestion.choices[x] === currentQuestion.answer) {
+    // SO SOMETHING ABOUT THE SCORE FOR A RIGHT ANSWER
+    score += secondsLeft
+    console.log(score)
+   }
+  
+   if (questions.length > currentQuestionIndex) {
+     displayQuestion(currentQuestionIndex)
+   } else {
+     stopGame()
+     
+   }
+
+
+}
+
+function stopGame() {
+  clearInterval(timerId)
+
+  questionElement.textContent = "Game is Over";
+  choiceA.textContent = "Your final score was:";
+  choiceB.textContent = score;
+  choiceC.textContent = "";
+  choiceD.textContent = "";
+  choiceE.textContent = "";
+
+
+  alert('the game is now over')
+}
   // set secondsLeft variable starting time (300 seconds = 5 minutes)
   // write secondsLeft to the page
 
@@ -29,34 +103,60 @@
   // show quiz-content element
 
   // display first question
-  displayQuestion(0);
+  // displayQuestion(0);
 
   // set timerIntervalId to setInterval function that decrements secondsLeft every second
-  timerIntervalId = setInterval(function() {
-    secondsLeft--;
-    if (secondsLeft <= 0) {
-      stopGame();
-    }
-  }, 1000);
+//   timerIntervalId = setInterval(function() {
+//     secondsLeft--;
+//     if (secondsLeft <= 0) {
+//       stopGame();
+//     }
+//   }, 1000);
 
-
+//  
 
 // create function to display a question and possible choices
 function displayQuestion(questionIndex) {
-  // check if questionIndex in questions array doesn't exist
-  if (!questions[questionIndex]) {
+  
+  currentQuestion = questions[questionIndex]
+  questionElement.textContent = currentQuestion.question;
+  choiceA.textContent = currentQuestion.choices[0];
+  choiceB.textContent = currentQuestion.choices[1];
+  choiceC.textContent = currentQuestion.choices[2];
+  choiceD.textContent = currentQuestion.choices[3];
+  choiceE.textContent = currentQuestion.choices[4];
+
+
+  // Question2 = questions[questionIndex ]
+  // questionElement.textContent = Question2.question;
+  // choiceA.textContent = currentQuestion.choices[0];
+  // choiceB.textContent = currentQuestion.choices[1];
+  // choiceC.textContent = currentQuestion.choices[2];
+  // choiceD.textContent = currentQuestion.choices[3];
+  // choiceE.textContent = currentQuestion.choices[4];
+
+}
+
+
+
+
+
+
+
+ // if (!questions[questionIndex]) {
     // stop game, we've hit last question
-    return stopGame();
-  }
+  //  return stopGame();
+  //} 
 
   // get questions[questionIndex]
   // print question to the page
   // use data attribute to know which index the question is
   // loop through choices and print out choices to the page (make them buttons)
 
-}
+
 
 // create function to handle users answering
+
   // use event delegation to make sure button was clicked
   // read data attribute of what question we answered (index)
   // check to see if choice picked is same as questions correct answer
@@ -74,6 +174,7 @@ function displayQuestion(questionIndex) {
   // show post-game-screen
   // print out user score
 
+startBtn.addEventListener('click',startBtnHandler);
 
 
 // add event listeners
@@ -81,3 +182,4 @@ function displayQuestion(questionIndex) {
   // quizcontent (for answering a question) -> use event delegation
   // play again button (for starting the game)
 
+  
